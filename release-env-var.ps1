6870e823d31773024ca7da55ca2bc894597d38ae
Levi Fuller <lfuller941@gmail.com>
2020-07-10 15:20:11 -0600

    Updated example json parsing release script

--- release-env-var.ps1
+++ release-env-var.ps1
@@ -1,2 +1,5 @@
-$envJson = get-content -raw -path './src/assets/config/app.config.json'| Convertfrom-Json | ConvertTo-Json -Depth 100
-(Get-Content -path './manifest-example.yml' -Raw) -replace 'MY_ENV_OBJ_HERE',$envJson
+$manifestPath='./manifest-example.yml'
+$outputPath='./manifest-test.yml'
+$envJson = get-content -raw -path './src/assets/config/app.config.json'| Convertfrom-Json | ConvertTo-Json -Compress -Depth 100
+((Get-Content -path $manifestPath -Raw) -replace 'MY_ENV_OBJ_HERE', $envJson) | Set-Content -Path $outputPath -Force
+# May be necessary - check: .Replace("`r`n","`n")
